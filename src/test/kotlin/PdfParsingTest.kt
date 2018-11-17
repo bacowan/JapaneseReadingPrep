53d39942373f22f7b0b9b387564e892b6b5c844f
Brendan Cowan <bacowan@ualberta.ca>
2018-11-17 15:02:47 -0400

    fixing up pdf parsing tests

--- src/test/kotlin/PdfParsingTest.kt
+++ src/test/kotlin/PdfParsingTest.kt
@@ -9,6 +9,11 @@ class PdfParsingTest {
         val parser = PdfParser()
         val pdfFile = File(javaClass.getResource("bocchan.pdf").file)
         val pdfText = parser.translateTextFromPdf(pdfFile)
-        Assert.assertTrue(pdfText.isNotEmpty())
+        Assert.assertEquals(5, pdfText.count())
+        Assert.assertTrue(pdfText[0].isNotBlank())
+        // don't expect anything from pdfText[1]
+        Assert.assertTrue(pdfText[2].isNotBlank())
+        Assert.assertTrue(pdfText[3].isNotBlank())
+        Assert.assertTrue(pdfText[4].isNotBlank())
     }
 }
