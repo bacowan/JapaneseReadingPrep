c500d3960d0035c969631830c8e4e0cbfc124b26
Brendan Cowan <bacowan@ualberta.ca>
2018-11-23 00:13:55 -0400

    Submit button switches to tab 2

--- frontend/main_page.js
+++ frontend/main_page.js
@@ -3,72 +3,122 @@
 const e = React.createElement;
 
 class Navbar extends React.Component {
-  render() {
-    return (
-      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-          <a class="navbar-brand" href="#">Japanese Reading Prep</a>
-          
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-              <span class="navbar-toggler-icon"></span>
-          </button>
-          
-          <div class="collapse navbar-collapse" id="collapsibleNavbar">
-              <ul class="navbar-nav ml-auto">
-                  <li class="nav-item ml-auto">
-                      <a class="nav-link" href="#">Login</a>
-                  </li>
-                  <li class="nav-item ml-auto">
-                      <a class="nav-link" href="#">Sign Up</a>
-                  </li>
-              </ul>
-          </div>
-      </nav>
-    )
-  }
+    render() {
+        return (
+            <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+                <a className="navbar-brand" href="#">Japanese Reading Prep</a>
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item ml-auto">
+                            <a className="nav-link" href="#">Login</a>
+                        </li>
+                        <li className="nav-item ml-auto">
+                            <a className="nav-link" href="#">Sign Up</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+class ParserSectionTab extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { active: props.active }
+    }
+
+    render() {
+        let classes = ["tab-pane", "container"]
+        if (this.state.active) {
+            classes.push("active")
+        }
+        else {
+            classes.push("fade")
+        }
+        return (
+            <div className={classes.join(' ')}>
+                {this.props.children}
+            </div>
+        )
+    }
 }
 
 class ParserSection extends React.Component {
-  render() {
-    return (
-      <div class="tab-content vertical-center">
-        <div class="tab-pane container active">
-            <h1 class="text-center">Put whatever you want to read here!</h1>
-            <p class="text-center mb-5 mb-sm-4 mb-md-5">Browse for a .pdf or .txt file, type in a website address, or paste some text</p>
-            <form class="container-fluid">
-                <div class="row">
-                    <div class="input-group-prepend col-sm-3 col-md-2 mb-3">
-                        <select class="custom-select">
-                            <option>File</option>
-                            <option>Website</option>
-                            <option>Text</option>
-                        </select>
+
+    constructor(props) {
+        super(props)
+        this.firstTabRef = React.createRef();
+        this.secondTabRef = React.createRef();
+        this.thirdTabRef = React.createRef();
+        this.handleParseSubmit = this.handleParseSubmit.bind(this)
+    }
+
+    handleParseSubmit() {
+        this.secondTabRef.current.click()
+    }
+
+    render() {
+        return (
+            <div>
+                <ul className="nav nav-tabs invisible">
+                    <li className="nav-item" ref={this.firstTabRef}>
+                        <a className="nav-link active" data-toggle="tab" href="#firstTab"></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" data-toggle="tab" href="#secondTab" ref={this.secondTabRef}></a>
+                    </li>
+                    <li className="nav-item" ref={this.thirdTabRef}>
+                        <a className="nav-link" data-toggle="tab" href="#thirdTab"></a>
+                    </li>
+                </ul>
+
+                <div className="tab-content vertical-center">
+                    <div className="tab-pane container active" id="firstTab">
+                        <h1 className="text-center">Put whatever you want to read here!</h1>
+                        <p className="text-center mb-5 mb-sm-4 mb-md-5">Browse for a .pdf or .txt file, type in a website address, or paste some text</p>
+                        <form className="container-fluid">
+                            <div className="row">
+                                <div className="input-group-prepend col-sm-3 col-md-2 mb-3">
+                                    <select className="custom-select">
+                                        <option>File</option>
+                                        <option>Website</option>
+                                        <option>Text</option>
+                                    </select>
+                                </div>
+                                <div className="custom-file col-sm-9 col-md-10 mb-3">
+                                    <input type="file" className="custom-file-input" id="customFile"></input>
+                                    <label className="custom-file-label" htmlFor="customFile">Choose file</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <button type="button" className="btn btn-primary btn-lg mx-auto" onClick={this.handleParseSubmit}>Create reading guide</button>
+                            </div>
+                        </form>
                     </div>
-                    <div class="custom-file col-sm-9 col-md-10 mb-3">
-                        <input type="file" class="custom-file-input" id="customFile"></input>
-                        <label class="custom-file-label" for="customFile">Choose file</label>
-                    </div>
+                    <div className="tab-pane container fade" id="secondTab">
+                        two
                 </div>
-                <div class="row">
-                    <button type="button" class="btn btn-primary btn-lg mx-auto">Create reading guide</button>
+                    <div className="tab-pane container fade" id="thirdTab">
+                        three
                 </div>
-            </form>
-        </div>
-        <div class="tab-pane container fade">
-            two
-        </div>
-        <div class="tab-pane container fade">
-            three
-        </div>
-      </div>
-    )
-  }
+                </div>
+            </div>
+        )
+    }
 }
 
 const domContainer = document.querySelector('#root');
 ReactDOM.render(
     <div>
-        <Navbar/>
-        <ParserSection/>
+        <Navbar />
+        <ParserSection />
     </div>,
     domContainer
 );
