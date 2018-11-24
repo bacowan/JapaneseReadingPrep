1ce36e8e404f8d1fd5edc97361442e4cd8d17e71
Brendan Cowan <bacowan@ualberta.ca>
2018-11-24 16:18:20 -0400

    refactoring each section in the main page into its own component

--- frontend/main_page.js
+++ frontend/main_page.js
@@ -70,7 +70,7 @@ class ReviewCard extends React.Component {
                         <i className="material-icons align-middle" align="center">access_time</i>
                     </button>
                 </div>
-                <ReviewCardModal kanji={this.props.kanji} definitions={this.props.definitions}/>
+                <ReviewCardModal kanji={this.props.kanji} definitions={this.props.definitions} />
             </div>
         )
     }
@@ -79,20 +79,20 @@ class ReviewCard extends React.Component {
 class ResultsSection extends React.Component {
     render() {
         const mockData = [
-            <ReviewCard kanji="猫" definitions="Cat" key="猫"/>,
-            <ReviewCard kanji="犬" definitions="Dog" key="犬"/>,
-            <ReviewCard kanji="好き" definitions="Like" key="好き"/>,
-            <ReviewCard kanji="頭" definitions="Head" key="頭"/>,
-            <ReviewCard kanji="卵" definitions="Egg" key="卵"/>,
-            <ReviewCard kanji="ピカチュウ" definitions="Pikachu" key="ピカチュウ"/>,
-            <ReviewCard kanji="水" definitions="Water" key="水"/>,
-            <ReviewCard kanji="上がる" definitions="To go up" key="上がる"/>,
-            <ReviewCard kanji="海" definitions="Ocean" key="海"/>,
-            <ReviewCard kanji="何" definitions="What" key="何"/>,
-            <ReviewCard kanji="頭がいい" definitions="Smart" key="頭がいい"/>,
-            <ReviewCard kanji="うんこ" definitions="Poop" key="うんこ"/>,
-            <ReviewCard kanji="音楽" definitions="Music" key="音楽"/>,
-            <ReviewCard kanji="青い" definitions="Blue" key="青い"/>
+            <ReviewCard kanji="猫" definitions="Cat" key="猫" />,
+            <ReviewCard kanji="犬" definitions="Dog" key="犬" />,
+            <ReviewCard kanji="好き" definitions="Like" key="好き" />,
+            <ReviewCard kanji="頭" definitions="Head" key="頭" />,
+            <ReviewCard kanji="卵" definitions="Egg" key="卵" />,
+            <ReviewCard kanji="ピカチュウ" definitions="Pikachu" key="ピカチュウ" />,
+            <ReviewCard kanji="水" definitions="Water" key="水" />,
+            <ReviewCard kanji="上がる" definitions="To go up" key="上がる" />,
+            <ReviewCard kanji="海" definitions="Ocean" key="海" />,
+            <ReviewCard kanji="何" definitions="What" key="何" />,
+            <ReviewCard kanji="頭がいい" definitions="Smart" key="頭がいい" />,
+            <ReviewCard kanji="うんこ" definitions="Poop" key="うんこ" />,
+            <ReviewCard kanji="音楽" definitions="Music" key="音楽" />,
+            <ReviewCard kanji="青い" definitions="Blue" key="青い" />
         ]
 
         return (
@@ -106,94 +106,116 @@ class ResultsSection extends React.Component {
     }
 }
 
-class ParserSection extends React.Component {
+class ResultsPage extends React.Component {
+    render() {
+        return (
+            <div className="container" id="thirdTab">
+                <ResultsSection/>
+            </div>
+        )
+    }
+}
 
+class LoadingPage extends React.Component {
     constructor(props) {
         super(props)
-        this.firstTabRef = React.createRef();
-        this.secondTabRef = React.createRef();
-        this.thirdTabRef = React.createRef();
-
         this.state = {progress: 0}
-
-        this.handleParseSubmit = this.handleParseSubmit.bind(this)
     }
 
-    handleParseSubmit() {
-        this.secondTabRef.current.click()
+    componentDidMount() {
         //temporary: this shouldn't be on a timer, it should update when the server tells it to
         this.timerID = setInterval(
             () => this.tick(),
             100
-          );
+        );
     }
 
-    //
-    //temporary: this shouldn't be on a timer, it should update when the server tells it to
-    tick() {
-        this.setState(state => ({
-          progress: state.progress + 10
-        }));
-        if (this.state.progress >= 100) {
-            clearInterval(this.timerID);
-            this.thirdTabRef.current.click()
-        }
-      }
-  
     componentWillUnmount() {
         clearInterval(this.timerID);
     }
-    //
-    //
+
+    tick() {
+        this.setState(state => ({
+            progress: state.progress + 10
+        }));
+        if (this.state.progress >= 100) {
+            clearInterval(this.timerID);
+            this.props.onLoadingDone()
+        }
+    }
 
     render() {
         return (
-            <div>
-                <ul className="nav nav-tabs invisible">
-                    <li className="nav-item">
-                        <a className="nav-link active" data-toggle="tab" href="#firstTab" ref={this.firstTabRef}></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" data-toggle="tab" href="#secondTab" ref={this.secondTabRef}></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" data-toggle="tab" href="#thirdTab" ref={this.thirdTabRef}></a>
-                    </li>
-                </ul>
+            <div className="container" id="firstTab">
+                <h1 className="text-center mb-3 display-1">Parsing...</h1>
+                <div className="progress" style={{ height: "30px" }}>
+                    <div className="progress-bar progress-bar-animated progress-bar-striped" style={{ width: this.state.progress.toString() + "%", height: "30px" }}></div>
+                </div>
+            </div>
+        )
+    }
+}
 
-                <div className="tab-content vertical-center">
-                    <div className="tab-pane container active" id="firstTab">
-                        <h1 className="text-center">Put whatever you want to read here!</h1>
-                        <p className="text-center mb-5 mb-sm-4 mb-md-5">Browse for a .pdf or .txt file, type in a website address, or paste some text</p>
-                        <form className="container-fluid">
-                            <div className="row">
-                                <div className="input-group-prepend col-sm-3 col-md-2 mb-3">
-                                    <select className="custom-select">
-                                        <option>File</option>
-                                        <option>Website</option>
-                                        <option>Text</option>
-                                    </select>
-                                </div>
-                                <div className="custom-file col-sm-9 col-md-10 mb-3">
-                                    <input type="file" className="custom-file-input" id="customFile"></input>
-                                    <label className="custom-file-label" htmlFor="customFile">Choose file</label>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <button type="button" className="btn btn-primary btn-lg mx-auto" onClick={this.handleParseSubmit}>Create reading guide</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div className="tab-pane container fade" id="secondTab">
-                        <h1 className="text-center mb-3 display-1">Parsing...</h1>
-                        <div className="progress" style={{height: "30px"}}>
-                            <div className="progress-bar progress-bar-animated progress-bar-striped" style={{width: this.state.progress.toString() + "%", height: "30px"}}></div>
+class InputPage extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <h1 className="text-center">Put whatever you want to read here!</h1>
+                <p className="text-center mb-5 mb-sm-4 mb-md-5">Browse for a .pdf or .txt file, type in a website address, or paste some text</p>
+                <form className="container-fluid">
+                    <div className="row">
+                        <div className="input-group-prepend col-sm-3 col-md-2 mb-3">
+                            <select className="custom-select">
+                                <option>File</option>
+                                <option>Website</option>
+                                <option>Text</option>
+                            </select>
+                        </div>
+                        <div className="custom-file col-sm-9 col-md-10 mb-3">
+                            <input type="file" className="custom-file-input" id="customFile"></input>
+                            <label className="custom-file-label" htmlFor="customFile">Choose file</label>
                         </div>
                     </div>
-                    <div className="tab-pane container fade" id="thirdTab">
-                        <ResultsSection/>
+                    <div className="row">
+                        <button type="button" className="btn btn-primary btn-lg mx-auto" onClick={this.props.handleParseSubmit}>Create reading guide</button>
                     </div>
-                </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+class ParserSection extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleParseSubmit = this.handleParseSubmit.bind(this)
+        this.handleLoadingDone = this.handleLoadingDone.bind(this)
+        this.state = {
+            progress: 0,
+            page: <InputPage handleParseSubmit={this.handleParseSubmit} />
+        }
+    }
+
+    handleParseSubmit() {
+        this.setState(
+            {
+                page: <LoadingPage onLoadingDone={this.handleLoadingDone}/>
+            }
+        )
+    }
+
+    handleLoadingDone() {
+        this.setState(
+            {
+                page: <ResultsPage/>
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div className="vertical-center">
+                {this.state.page}
             </div>
         )
     }
